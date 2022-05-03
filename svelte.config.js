25ca17596d4c7687acb3e52f497a3231583e062a
Michael Wooley <wm.wooley@gmail.com>
2022-05-03 19:50:50 -0400

    Update config + tsconfig paths

--- svelte.config.js
+++ svelte.config.js
@@ -16,13 +16,11 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
+			// We don't need any of netlify's edge functions, etc.
+			//
+			// We need netlify (or something beyond github pages) because it allows us to send
+			// the response headers required by SharedArrayBuffer.
 			edge: false,
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
 			split: false
 		}),
 
