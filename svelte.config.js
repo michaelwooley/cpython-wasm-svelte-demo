51fd68f0fe8f35246fb5a6991b2cd730fbb29ed6
Michael Wooley <wm.wooley@gmail.com>
2022-05-03 19:02:30 -0400

    Update svelte.config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
+import { resolve } from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,14 +15,39 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
+		}),
 
 		vite: {
+			resolve: {
+				alias: {
+					$components: resolve('./src/components'),
+					$containers: resolve('./src/containers')
+				}
+			},
+
 			css: {
 				preprocessorOptions: {
 					scss: {
 						additionalData: '@use "src/variables.scss" as *;'
 					}
+				}
+			},
+
+			server: {
+				headers: {
+					// Needed to allow SharedArrayBuffer to squeak through on some browsers....
+					// REFERENCE https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/SharedArrayBuffer#security_requirements
+					'Cross-Origin-Opener-Policy': 'same-origin',
+					'Cross-Origin-Embedder-Policy': 'require-corp'
 				}
 			}
 		}
