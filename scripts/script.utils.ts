f90fb568d8f5fd4c4c18bb85a335cc11023320fb
Michael Wooley <wm.wooley@gmail.com>
2022-05-03 23:55:04 -0400

    More tweaks to script

--- scripts/script.utils.ts
+++ scripts/script.utils.ts
@@ -1,6 +1,4 @@
-import { $, quiet, path } from 'zx';
-
-export const baseDir = path.resolve('.');
+import { $, quiet } from 'zx';
 
 export const formatDir = async (dir: string): Promise<void> => {
 	await quiet(
@@ -10,3 +8,16 @@ export const formatDir = async (dir: string): Promise<void> => {
 
 export const logWithEmoji = (msg: string, emoji?: string): void =>
 	console.log(((emoji || ' ') + ' '.repeat(3)).slice(0, 10) + msg);
+
+const getBaseDir = (): string => {
+	const _baseDir = process.env.npm_config_local_prefix;
+
+	if (!_baseDir) {
+		throw new Error('ENVVAR "npm_config_local_prefix" is not defined?!');
+	}
+
+	return _baseDir;
+};
+
+export const baseDir = getBaseDir();
+await quiet($`cd ${baseDir}`);
