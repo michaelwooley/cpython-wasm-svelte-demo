0cabaca325856370143a0b82b74da226135f6999
Michael Wooley <wm.wooley@gmail.com>
2022-05-03 21:02:36 -0400

    Add scripts setup

--- src/lib/wasm/add.post.emcc.js
+++ src/lib/wasm/add.post.emcc.js
@@ -1,5 +1,4 @@
 /// <reference types="emscripten" />
-/// <reference types="./add" />
 /** Above will import declarations from @types/emscripten, including Module etc. */
 /** It is not .ts file but declaring reference will pass TypeScript Check. */
 
