bbe22ff089dbf8ff3c9d92c4d72b51d4c6f19c8c
Michael Wooley <wm.wooley@gmail.com>
2022-05-11 01:22:05 -0400

    Check in expansion on add work

--- src/lib/wasm/add.c
+++ src/lib/wasm/add.c
@@ -1,4 +1,42 @@
+
+
+#include <pthread.h>
+#include <emscripten.h>
+#include <emscripten/html5.h>
+#include <emscripten/threading.h>
+#include <stdio.h>
+
 int add(int a, int b)
 {
     return a + b;
+}
+
+void sleep(double msecs)
+{
+    double t1 = emscripten_get_now();
+    emscripten_thread_sleep(msecs);
+    double t2 = emscripten_get_now();
+    printf("emscripten_thread_sleep() slept for %f msecs.\n", t2 - t1);
+}
+
+void *thread_main(void *arg)
+{
+    EM_ASM(out('hello from thread!'));
+
+    sleep(1000);
+    int is_main = emscripten_is_main_browser_thread();
+    int is_runtime = emscripten_is_main_runtime_thread();
+    printf("is main thread? %d, runtime? %d\n", is_main, is_runtime);
+    EM_ASM(out('hello from thread again...'));
+    emscripten_force_exit(0);
+    __builtin_trap();
+}
+
+int main()
+{
+    // pthread_t thread;
+    // pthread_create(&thread, NULL, thread_main, NULL);
+    thread_main(NULL);
+    emscripten_exit_with_live_runtime();
+    __builtin_trap();
 }
